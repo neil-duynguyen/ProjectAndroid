4e865533df47ef2dfc4845e4d04164bae168573d
@neil.duynguyen <neil.duynguyen@gmail.com>
2023-07-21 23:37:45 +0700

    Fix chat provider

--- app/src/main/java/com/example/projectandroid/providerFragments/ChatFragmentProvider.java
+++ app/src/main/java/com/example/projectandroid/providerFragments/ChatFragmentProvider.java
@@ -2,65 +2,122 @@ package com.example.projectandroid.providerFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.projectandroid.R;
+import com.example.projectandroid.adapters.ChatAdapter;
+import com.example.projectandroid.model.Message;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChatFragmentProvider#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ChatFragmentProvider extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ListView listView;
+    private EditText editTextMessage;
+    private Button btnSend;
+    private DatabaseReference databaseReference;
+    private List<Message> messages;
+    private String currentUserID;
+    private ChatAdapter chatAdapter;
 
     public ChatFragmentProvider() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragmentProvider.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChatFragmentProvider newInstance(String param1, String param2) {
-        ChatFragmentProvider fragment = new ChatFragmentProvider();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        // Initialize Firebase Database reference
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("messages");
+        messages = new ArrayList<>();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            currentUserID = currentUser.getEmail();
+        } else {
+            // Handle the case when the user is not signed in or authentication fails
+            currentUserID = null;
         }
+        chatAdapter = new ChatAdapter(messages, currentUserID);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_provider, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat_provider, container, false);
+        listView = view.findViewById(R.id.list_viewp);
+        listView.setAdapter(chatAdapter);
+        editTextMessage = view.findViewById(R.id.edit_text_messagep);
+        btnSend = view.findViewById(R.id.btn_sendp);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Listen for new messages in the Firebase Database
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+                Message message = dataSnapshot.getValue(Message.class);
+                messages.add(message);
+                chatAdapter.notifyDataSetChanged();
+                listView.smoothScrollToPosition(messages.size() - 1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+
+        // Send button click listener
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editTextMessage.getText().toString();
+                if (!message.isEmpty()) {
+                    sendMessage(message);
+                    editTextMessage.setText("");
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        if (currentUserID != null) {
+            // Push the new message to the Firebase Database
+            String key = databaseReference.push().getKey();
+            Message newMessage = new Message(currentUserID, message);
+            databaseReference.child(key).setValue(newMessage);
+        } else {
+            // Handle the case when the user is not signed in or authentication fails
+        }
     }
 }
